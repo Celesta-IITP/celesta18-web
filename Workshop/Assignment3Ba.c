1f6053b3a160e6af20ff1f2bf515cd011edae495
ivary43 <contactmanish16@gmail.com>
2017-10-13 22:58:15 +0530

    added bla bla

---
+++ Workshop/Assignment3Ba.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+
+int n ;
+printf("Enter the number of names");
+scanf("%d",&n);
+char names[n][6] ;
+printf("Enter the names \n");
+int loop ;
+	for ( loop=0 ;loop<n ;++loop)
+		{
+			scanf("%s",&names[loop]) ;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0 ;
+}
