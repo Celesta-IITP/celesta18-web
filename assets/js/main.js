e9932ab396d60ed27e4708d3de6534f20fc88cab
deepanjan <deepanjan052000@gmail.com>
2018-08-03 00:12:54 +0530

    Changed the whole home page to make it better and responsive to all devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,89 +1,73 @@
-/*
-	Radius by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
-(function($) {
-
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
+jQuery(document).ready(function($){
+	//open menu
+	$('.cd-menu-trigger').on('click', function(event){
+		event.preventDefault();
+		$('#cd-main-content').addClass('move-out');
+		$('#main-nav').addClass('is-visible');
+		$('.cd-shadow-layer').addClass('is-visible');
+	});
+	//close menu
+	$('.cd-close-menu').on('click', function(event){
+		event.preventDefault();
+		$('#cd-main-content').removeClass('move-out');
+		$('#main-nav').removeClass('is-visible');
+		$('.cd-shadow-layer').removeClass('is-visible');
 	});
 
-	$(function() {
-
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$footer = $('#footer');
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Header.
-			$header.each( function() {
-
-				var t 		= jQuery(this),
-					button = t.find(' .common');     //*************************** this needs to be changed with a cross mark in my site
-
-				t.toggleClass('hide');
-				button.click(function(e) {
-
-					t.toggleClass('hide');
-
-					if ( t.hasClass('preview') ) {
-						return true;
-					} else {
-						e.preventDefault();
-					}
-
-				});
-
-			});
-
-			$header.click(function(evt){
-				var t = document.getElementbyId("data");
-				if(evt.target.class=="inner" && !t.hasClass('hide')){}
-				else {
-					t.toggleClass('hide');
-				}
-			});
-
-		// Footer.
-			$footer.each( function() {
-
-				var t 		= jQuery(this),
-					inner 	= t.find('.inner'),
-					button 	= t.find('.info');
-
-				button.click(function(e) {
-					t.toggleClass('show');
-					e.preventDefault();
-				});
-
-			});
-
+	//clipped image - blur effect
+	set_clip_property();
+	$(window).on('resize', function(){
+		set_clip_property();
 	});
 
-})(jQuery);
+	function set_clip_property() {
+		var $header_height = $('.cd-header').height(),
+			$window_height = $(window).height(),
+			$header_top = $window_height - $header_height,
+			$window_width = $(window).width();
+		$('.cd-blurred-bg').css('clip', 'rect('+$header_top+'px, '+$window_width+'px, '+$window_height+'px, 0px)');
+	}
+
+	setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 3000);
+
+    // loading printing celesta --------------------------------------------------------------
+
+    // Wrap every letter in a span
+	$('.ml1 .letters').each(function(){
+  		$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+	});
+
+	anime.timeline({loop: true})
+  		.add({
+    	targets: '.ml1 .letter',
+  		scale: [0.3,1],
+    	opacity: [0,1],
+    	translateZ: 0,
+    	easing: "easeOutExpo",
+    	duration: 1500,
+    	delay: function(el, i) {
+    		return 70 * (i+1)
+    	}
+  	}).add({
+    	targets: '.ml1 .line',
+    	scaleX: [0,1],
+    	opacity: [0.5,1],
+    	easing: "easeOutExpo",
+    	duration: 700,
+   		offset: '-=875',
+    	delay: function(el, i, l) {
+    		return 80 * (l - i);
+   		}
+  	}).add({
+    	targets: '.ml1',
+    	opacity: 0,
+    	duration: 1000,
+    	easing: "easeOutExpo",
+    	delay: 1000
+  	});
+
+  	// nd loading ----------------------------------------------------------------------------
+
+});
--- index.php
+++ index.php
@@ -1,565 +1,163 @@
 <?php
-session_start();
+	session_start();
 ?>
+
 <!DOCTYPE html>
- 
-<html lang="en">
-
-  <head>
-
-    <meta name="google-site-verification" content="qUmyqM9K6PlBm7mKg3Rpk0JniY6W5_stkbtOFozfgAo" />
-
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-    <title>Celesta</title>
-
-    <meta name="description" content="Celesta, IIT Patna Technical Fest">
-
-    <meta name="author" content="Creatives IIT Patna">
-
-    <meta name="keywords" content="celesta iitp, celesta iit patna, celesta.org.in, Celesta, IITP, IIT Patna, Indian Institute of Technology Patna, Technical Fest IIT Patna">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="assets/css/main.css" />
-
-    <link rel="stylesheet" href="./css/animation.css">
-
-    <link rel="stylesheet" href="./css/demo.css">
-
-    <link rel="stylesheet" href="./css/loader.css">
-    <style type="text/css">
-
-/*=============================== Basic ===============================*/
-
-        body {
-      background-image: url("../img/bg.png");
-      overflow: hidden;
-    }
-
-    .visible {display: block;}
-
-    .layer {display: none;}
-
-    .layer.one {
-
-      transition-property: background;
-      transition-timing-function: ease-in;
-      transition: 1.2s;
-      display: block;
-
-      background: url("images/bg_img.jpg");
-
-      background-size: cover;
-
-    }
-
-    #main {
-      transition-property: opacity;
-      transition-timing-function: ease-in;
-      transition: 1.5s;
-      opacity: 0;
-      height: 100%;
-    }
-
-
-
-    #home {padding: 0;}
-
-    #home canvas {}
-
-          #particles {
-
-            width: 100%;
-
-            height: 100%;
-
-            overflow: hidden;
-
-          }
-
-          #intro {
-
-            position: absolute;
-
-            left: 0;
-
-            top: 50%;
-
-            padding: 0 20px;
-
-            width: 100%;
-
-            text-align: center;
-
-          }
-
-          /* Add Zoom Animation */
-
-          .animate {
-
-            -webkit-animation: animatezoom 0.6s;
-
-            animation: animatezoom 0.6s
-
-          }
-          
-
-    /*=========================== Media Queries ============================*/
-
-    @media screen and (max-width:680px) {
-
-      ul.links li:not(:first-child) {display: none;}
-
-      ul.links li.icon {
-
-        float: right;
-
-        display: inline-block;
-
-      }
-
-      body {
-
-        font-size: 120%;
-
-      }
-
-    }
-
-    @media screen and (max-width:768px) {
-
-      body {
-
-        font-size: 120%;
-
-      }
-
-    }
-
-    @media screen and (max-width:680px) {
-
-      ul.links.responsive {position: relative;}
-
-      ul.links.responsive li.icon {
-
-        position: absolute;
-
-        right: 0;
-
-        top: 0;
-
-      }
-
-      ul.links.responsive li {
-
-        float: none;
-
-        display: inline;
-
-      }
-
-      ul.links.responsive li a {
-
-        display: block;
-
-        text-align: left;
-
-      }
-
-    }
-
-    /* Change styles for span and cancel button on extra small screens */
-
-    @media screen and (max-width: 400px) {
-
-        .modal-content {
-
-          width: 95%;
-
-        }
-
-        span.psw {
-
-           display: block;
-
-           float: none;
-
-        }
-
-        .cancelbtn {
-
-           width: 100%;
-
-        }
-         .clsbtn {
-           position: fixed;
-           top:0;
-           right:0;
-           display: block;
-           cursor: pointer;
-           width: 30px;
-       height:30px;
-        }
-        .opened {
-           display: block;
-           position: fixed;
-           height: 100%;
-           width: 100%;
-
-        }
-       
-
-
-      }
-
-    /*======================================================================*/
-#live a{
-  text-decoration: none;
-  color:#FFFFFF;
-}
-#live a:hover{
-  text-decoration: none;
-  color:#3cfff9;
-}
-    </style>
-
-
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-
-</head>
-<style type="text/css">
-body{
-  background: linear-gradient(to right, #bab9b9,#bab9b9,blue,white,white,blue,#bab9b9,#bab9b9);
-  background : white;
-  background-size: 100% auto;
-  overflow: hidden;
-}
-
-@media screen and (max-width: 1400px) {
-
-      #footer {
-        position: relative;
-        bottom: 0;
-      }
-
-        #footer .inner {
-          height: auto;
-          display: block;
-        }
-
-          #footer .inner .content,
-          #footer .inner .copyright {
-            display: block;
-            width: 100%;
-            text-align: center;
-          }
-
-        #footer .info {
-          display: none;
-        }
-
-    }
-
-</style>
-<body>
-
-<div id="loading">
-
-      <div id="loading-center">
-
-        <div id="loading-center-absolute">
-
-          <div class="object" id="first_object"></div>
-
-          <div class="object" id="second_object"></div>
-
-          <div class="object" id="third_object"></div>
-
-        </div>
-
-      </div>
-
-</div>
-
-    <script type="text/javascript">
-
-      $(window).load(function(){
-
-        $('#loading').delay(350).fadeOut("slow", function(){
-            $('#main').css("opacity", "1");
-            $('.layer.one').css("background", "#fff");
-            $('#particles').particleground({
-
-              dotColor: 'rgba(0,0,255,0.27)',
-
-              lineColor: 'rgba(255,0,0,0.12)',
-
-              density: 7500
-
-              });
-        });
-      
-     });   
-
-      </script>
-
-    <!-- Scripts -->
-      <script src="assets/js/jquery.min.js"></script>
-      <script src="assets/js/skel.min.js"></script>
-      <script src="assets/js/util.js"></script>
-      <script src="assets/js/main.js"></script>
-
-<!--<div id="live" style="
-    top: 50px;
-    position: absolute;
-    z-index: 10;
-    font-size: 20px;
-    background: rgba(0,0,0,0.4);
-    padding: 10px;
-    border-bottom-right-radius: 15px;
-    border-top-right-radius: 15px;
-">Upcoming events:<br><a href="Workshop/ai.htm" >->Workshop</a></div>-->
-      <script type="text/javascript"></script>
-        <article class="kontext capable animate">
-
-          <div class="layer one right show">
-
-            <!-- Header -->
-      <header id="header">
-        <div class="close common" style="position: absolute; z-index: 2000; ">&times</div>
-        <div class="inner">
-          <div class="content">
-            <h1>Home</h1><br>
-            <h2>
-            <div id="events">Events</div>
-            <!-- <div id="Workshop">Workshop</div> -->
-            <!-- <div id="Schedule">Schedule</div> -->
-            <!-- <div id="Team">Team</div> -->
-            <!-- <div id="Sponsors">Sponsors</div> -->
-            <!-- <div id="Gallery">Gallery</div> -->
-            <a href="/ca/"><div id="ca">Campus Ambassador</div></a>
-            </h2><br>
-            <?php
+<html lang="en" class="no-js">
+	<head>
+		<meta name="google-site-verification" content="qUmyqM9K6PlBm7mKg3Rpk0JniY6W5_stkbtOFozfgAo" />
+    	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    	<title>Celesta</title>
+    	<meta name="description" content="Celesta, IIT Patna Technical Fest">
+    	<meta name="author" content="Creatives IIT Patna">
+    	<meta name="keywords" content="celesta iitp, celesta iit patna, celesta.org.in, Celesta, IITP, IIT Patna, Indian Institute of Technology Patna, Technical Fest IIT Patna">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="assets/css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/component1.css" />
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/main.css">
+		<script src="assets/js/modernizr-2.6.2.min.js"></script>
+		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-7243260-2']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+
+	</head>
+	<body>
+	<div style="background: url('images/bg_img.jpg') no-repeat center; background-size: 95% auto; border: 1% solid white; width: 100%; height: 100%;">
+		<!-- LOADING PART ------------------------------------------------------------------------------------->
+		<div class="loading-page">
+  			<div class="counter">
+    			<h2 class="ml1" style="font-size: 1.5em; width: 500px; margin-left: -150px;">
+  					<span class="text-wrapper">
+    				<span class="line line1"></span>
+    				<span class="letters">WELCOME TO CELESTA</span>
+    				<h2 class="letters" style="font-size: 2em; font: pattaya; color: red;">REMASTERED</h2>
+    				<span class="line line2"></span>
+  					</span>
+				</h2>
+				<h1>0%</h1>
+    			<hr/>
+  			</div>		
+    		<div class="loader-section section-left"></div>
+    		<div class="loader-section section-right"></div>
+		</div>
+		<!-- LOADING PART END -------------------------------------------------------------------------------->
+
+		<!-- CANVAS ON BACKGROUND ---------------------------------------------------------------------------->
+		 
+		<!-- CANVAS END -------------------------------------------------------------------------------------->
+		
+		<div id="particles" style="position: fixed; width: 100%; height: 100%; z-index: 3;"></div>
+		<div class="container">
+			
+			<!-- Top Navigation -->
+			<div class="codrops-top clearfix">
+				<a class="codrops-icon" id="nouse"><span></span></a>
+				<span onclick="myFunction()" class="right" id="user1" style="background-color: rgba(0,0,0,0.8);"><a class="codrops-icon"><span>
+				<?php
                     if(isset($_SESSION['uid'])){
                       
                       $name = explode(" ",$_SESSION['name']) ;
 
-                      echo "Hi ".$name[0]."! <div class=\"button big alt\" id=\"logout\"><span>Log Out</span></div>";
+                      echo "Hi ".$name[0]."!<a href="login.php?act=logout" id="signup">Logout</a>";
                     
                     }
                     
                     else {
                       
-                        echo "<div class=\"button big alt\" id=\"login\"><span>Login</span></div>
-                        <b><br>Haven't registered yet <br><div class=\"button big alt cd-signup\" data-page=\"register\" id=\"register\"><span>Register</span></div></b>";
+                        echo "<a href="login.php" id="login">login</a><a href="register.php" id="signup">signup</a>";
                     
                     }
-                ?> 
-          </div>
-          <a href="#" id="data" class="button hidden common"><span>Let's Go</span></a>
-        </div>
-      </header>
+                ?>  
+            	</span></a></span>
+			</div>
+			
+				
+			<header style="display: none; color: white; font-size: 2.5em; width: 100%; position: absolute; z-index: 4;">
+				<!--header-->
+			</header>
+			<section>
+			</section>
+			<div class="component">
+				<!-- Start Nav Structure -->
+				<button class="cn-button" id="cn-button">MENU</button>
+				<div class="cn-wrapper" id="cn-wrapper">
+				    <ul>
+				      <li><a id="gallery" href="gallery.php"><span class="icon-picture"></span></a></li>
+				      <li><a id="ca" href="ca/index.php"><span class="icon-star"></span></a></li>
+				      <li><a id="home" href="index.html"><span class="icon-home"></span></a></li>
+				      <li><a id="events" href="event.php"><span class="icon-calendar"></span></a></li>
+				      <li><a id="about" href="about.html"><span class="icon-envelope-alt"></span></a></li>
+				     </ul>
+				</div>
+				<div id="cn-overlay" class="cn-overlay" style="padding-top: 2%;"></div>
+				<!-- End Nav Structure -->
+			</div>
+		</div><!-- /container -->
+	</div>
+		<script src="assets/js/polyfills.js"></script>
+		<script src="assets/js/demo1.js"></script>
+		<!-- For the demo ad only -->   
+<script src="assets/js/index.js"></script>
+<script src="assets/js/main.js"></script>
+<script type='text/javascript' src='./js/jquery.particleground.min.js'></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+<script>
+	$(document).ready(function(){
+		$("#home").mouseenter(function(){
+			$("#cn-overlay").html("<center><strong><h1>HOME</h1></strong><center>");
+		}).mouseleave(function(){
+			$("#cn-overlay").html("");
+		});
 
-    <!-- Main -->
-          <div id="main">
-              <div class="inner">
-                <div id="particles" style="position: absolute; z-index: 2;"></div>
-                <img align="middle" style="width: 100%; " src="images/bg_img.jpg">
-              </div>
-          </div>
+		$("#gallery").mouseenter(function(){
+			$("#cn-overlay").html("<center><strong><h1>GALLERY</h1></strong><center>");
+		}).mouseleave(function(){
+			$("#cn-overlay").html("");
+		});
 
-          <div id="intro">
-            
-          </div>
-       
-          </div>
+		$("#ca").mouseenter(function(){
+			$("#cn-overlay").html("<center><strong><h1>CAMPUS AMBASSADOR</h1></strong><center>");
+		}).mouseleave(function(){
+			$("#cn-overlay").html("");
+		});
 
-          <div class="layer two right">
+		$("#events").mouseenter(function(){
+			$("#cn-overlay").html("<center><strong><h1>EVENTS</h1></strong><center>");
+		}).mouseleave(function(){
+			$("#cn-overlay").html("");
+		});
 
-            <div id="events" class="content">
-
-              
-
-            </div>
-
-          <div class="dimmer"></div></div>
-
-          <div class="layer three right hide">
-
-            <div class="content" id="spons">
-
-         
-
-          </div>
-
-          <div class="dimmer"></div></div>
-
-          <div class="layer four right">
-
-            <div class="content" id="contacts">
-
-         
-
-          </div>
-
-          <div class="dimmer"></div></div>
-
-        </article>
-
-        <!-- Footer -->
-      <footer id="footer">
-        <a href="#" class="info fa fa-info-circle"><span>About</span></a>
-        <div class="inner">
-          <div class="content">
-            <h3>About Celesta 2k18</h3>
-            <p>Celesta is the annual Techno-Management Fest of IIT Patna. To promote technical and managerial enthusiasm amongst young and bright minds of our nation and to provide a platform to transform their innovative ideas into a meaningful realty. Celesta brings forth the perfect opportunity to do so.</p>
-          </div>
-          <div class="copyright">
-            <h3>Follow us</h3>
-            <ul class="icons">
-              <li><a href="https://twitter.com/celesta_iitp"  class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-              <li><a href="https://www.facebook.com/CelestaIITP/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-              <li><a href="https://www.instagram.com/celestaiitp_official/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-            </ul>
-          
-          </div>
-        </div>
-      </footer>
-
-        <script type='text/javascript' src='./js/jquery.particleground.min.js'></script>
-
-        <script src="./js/transitions.js"></script>
-
-        <!--<ul class="bullets"><li class="active" index="0"></li><li class="" index="1"></li><li class="" index="2"></li></ul>-->
-
-        <script type="text/javascript">
+		$("#about").mouseenter(function(){
+			$("#cn-overlay").html("<center><strong><h1>ABOUT</h1></strong><center>");
+		}).mouseleave(function(){
+			$("#cn-overlay").html("");
+		});
 
 
+		$('#particles').particleground({
 
-          var k = kontext( document.querySelector( '.kontext' ) );
+              dotColor: 'rgba(0,0,255,0.37)',
 
+              lineColor: 'rgba(255,0,0,0.22)',
 
-          function loader(Id ,pageUrl){
+              density: 10000
 
-            $(Id).load(pageUrl);
+              });
 
-          }
-
-          $(document).ready(function() {            
-
-            $('.intro').css({
-
-              'margin-top': -($('.intro').height() / 2)
-
-            });
-
-            function transit(n) {
-                k.show(n);
-            }
-
-            function load_page(url) {
-                window.location = url;
-            }
-
-            /*var home = links.eq(1);
-
-            var spons = links.eq(2);
-
-            var contacts = links.eq(3);
-
-            var register = links.eq(4);
-
-            home.on("click",function() {
-
-              nav2();
-
-              k.show(0);
-
-            });*/
-
-            $("#events").on("click",function() {
-                transit(1);
-                setTimeout(function() {
-                  load_page("event.php");
-                },400);
-            });
-
-
-            $("#Workshop").on("click",function() {
-                transit(2);
-                setTimeout(function() {
-                  load_page("event.html");
-                },400);
-            });
-
-
-            $("#Schedule").on("click",function() {
-                transit(3);
-                setTimeout(function() {
-                  load_page("event.html");
-                },400);
-            });
-
-
-            $("#Team").on("click",function() {
-                transit(4);
-                setTimeout(function() {
-                  load_page("event.html");
-                },400);
-            });
-
-
-            $("#Sponsors").on("click",function() {
-                transit(5);
-                setTimeout(function() {
-                  load_page("event.html");
-                },400);
-            });
-
-
-            $("#Gallery").on("click",function() {
-                transit(6);
-                setTimeout(function() {
-                  load_page("event.html");
-                },400);
-            });
-           
-            $("#Gallery").on("click",function() {
-                transit(1);
-                setTimeout(function() {
-                  load_page("ca/index.html");
-                },400);
-            }); 
-            
-            $("#logout").on("click",function() {
-                transit(1);
-                setTimeout(function() {
-                  load_page("login.php?act=logout");
-                },400);
-            });
-
-
-            $("#login").on("click",function() {
-                transit(1);
-                setTimeout(function() {
-                  load_page("login.php");
-                },400);
-            });
-
-
-            $("#register").on("click",function() {
-                transit(1);
-                setTimeout(function() {
-                  load_page("register.php");
-                },400);
-            });
-
-          });
-
-        </script>
-
-        <script type="application/ld+json">
+	});
+</script>
+<script type="application/ld+json">
 
         {
 
@@ -628,5 +226,5 @@ body{
   gtag('config', 'UA-121974846-1');
 </script>
 
-</body>
+	</body>
 </html>
