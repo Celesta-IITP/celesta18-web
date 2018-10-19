cc4c3345ace3abbf029c008a4ab4ad50463318ee
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2018-10-19 11:15:52 +0530

    rebuildjson

--- eventdata/rebuildJSON.php
+++ eventdata/rebuildJSON.php
@@ -1,6 +1,6 @@
 <?php
 include '../apiLe/defines.php';
-// $eventsSheetUtl
+// $eventsSheetUtl = 
 // todo insert above variable as parameter in below function when things are ok.
 $csvContents = file_get_contents($eventsSheetUtl);
 $csvContents  = explode(";;;",$csvContents);
@@ -24,25 +24,31 @@ $eveIndex = [0,0,0,0,0,0,0,0];
 // ];
 
 $attr = ["","name","about","venue","organised","contact","time","img","rules","eligibile","","date","catagory"];
+function format($inpStr){
+    $retStr = $inpStr;
+    $retStr = str_replace("\n","<br>",$retStr);
+    return $retStr;
+}
 foreach ($csvContents as $key => $value) {
     if ($key == 0)
-        continue;
-    echo $key."> ";
+    continue;
+    // echo $key."> ";
     $row = str_getcsv($value);
+    echo $row[1]."\n";
     if (!isset($row[12]))
         continue;
     $jsonDataOut = [];
     for ($i=0; $i < 13; $i++) { 
         if ($row[$i] && $row[$i]!="")
-            $jsonDataOut[$attr[$i]] = $row[$i];
+            $jsonDataOut[$attr[$i]] = format($row[$i]);
     }
     $eveIndex[$eveCatID[$row[12]]]++;
     unset($jsonDataOut[""]);
     $fileName = "1".str_pad($eveCatID[$row[12]], 2, "0",STR_PAD_LEFT).str_pad($eveIndex[$eveCatID[$row[12]]], 2, "0",STR_PAD_LEFT).".json";
-    print($fileName."\t".json_encode($row)."\t".$row[12]."\n");
-    // $fp = fopen( $fileName, 'w');
-    // fwrite($fp, json_encode($jsonDataOut));
-    // fclose($fp);
+    // print($fileName."\t".json_encode($row)."\t".$row[12]."\n");
+    $fp = fopen( $fileName, 'w');
+    fwrite($fp, json_encode($jsonDataOut));
+    fclose($fp);
 }
 
 ?>
