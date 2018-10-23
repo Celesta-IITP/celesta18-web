bb00412b3da6f6b9b772d15db087f7831b50d1f4
Piyush Chauhan <chauhanpiyush@hotmail.com>
2018-10-23 22:53:04 +0530

    Fixed events tab from gallery page

--- gallery.php
+++ gallery.php
@@ -256,7 +256,7 @@
 					<li><a href="index.php">Home</a></li>
 					<li><a href="index.php#about">About</a></li>
 					<li><a href="#">Gallery</a></li>
-					<li><a href="events.php">Events</a></li>
+					<li><a href="index.php#events">Events</a></li>
 					<li><a href="ca/index.php">Campus Ambassador</a></li>
 					<li><a href="spons.php">Sponsors</a></li>
 					<li><a href="team.php">Team</a></li>
